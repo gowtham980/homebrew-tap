8c3da72dc8c124a659f7a9766e923462acb7b42a
gowtham980 <gowthamkrishnateja@gmail.com>
2026-07-05 06:57:48 +0530

    feat: switch to DMG for macOS app distribution

--- Casks/clipper.rb
+++ Casks/clipper.rb
@@ -2,7 +2,7 @@ cask "clipper" do
   version "1.0.0"
   sha256 "eee7489e17d227c4d856756c209c89bba19ab318d7365146a9f8abc86661ee76"
 
-  url "https://github.com/gowtham980/Clipper/releases/download/v#{version}/Clipper-#{version}.zip"
+  url "https://github.com/gowtham980/Clipper/releases/download/v#{version}/Clipper-#{version}.dmg"
   name "Clipper"
   desc "Native macOS clipboard manager"
   homepage "https://github.com/gowtham980/Clipper"
